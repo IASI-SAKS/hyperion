a61bf3e42c216a9bc0308dde2729d39f64bafe57
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2020-10-23 13:35:33 +0200

    [WIP] Refactor datalog facts
    Still working on path conditions.
    
    Signed-off-by: Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>

--- analyse/src/main/java/it/cnr/saks/sisma/testing/InformationLogger.java
+++ analyse/src/main/java/it/cnr/saks/sisma/testing/InformationLogger.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import jbse.bc.ClassFile;
 import jbse.bc.Signature;
+import jbse.common.Type;
 import jbse.mem.*;
 import jbse.mem.exc.FrozenStateException;
 import jbse.mem.exc.InvalidNumberOfOperandsException;
@@ -19,6 +20,7 @@ import java.lang.reflect.Field;
 import java.util.*;
 
 import static jbse.algo.Util.valueString;
+import static jbse.apps.Util.LINE_SEP;
 import static jbse.common.Type.splitParametersDescriptors;
 
 public class InformationLogger {
@@ -135,6 +137,8 @@ public class InformationLogger {
                             + methodCall.getClassName() + ":" + methodCall.getMethodName() + ":" + methodCall.getMethodDescriptor() + ", "
                             + methodCall.getParameterSet().getParameters() + ")");
 
+                    this.datalogOut.println("\n" + methodCall.getPathCondition() + "\n");
+
 //                    this.datalogOut.println("uses(" + klass + ":" + method + ", " + methodCall.getClassName() + ")");
 
 //                    for (ListIterator<TestInformation.MethodCall> i2 = methodCalls.listIterator(iterator.nextIndex()); i2.hasNext(); ) {
@@ -194,7 +198,14 @@ public class InformationLogger {
 
     private void inspectMethodCall(State s, String name, Signature method, ClassFile classFile, String pathId, String programPoint) {
 
-        TestInformation.MethodCall md = this.loggedInformation.get(this.currClass).get(this.currMethod).addMethodCall(name, method.getDescriptor(), classFile.getClassName(), pathId, programPoint);
+        StringBuilder sb = new StringBuilder();
+        try {
+            formatPathCondition(s, sb);
+        } catch (FrozenStateException e) {
+            e.printStackTrace();
+        }
+
+        TestInformation.MethodCall md = this.loggedInformation.get(this.currClass).get(this.currMethod).addMethodCall(name, method.getDescriptor(), classFile.getClassName(), pathId, programPoint, sb.toString());
         Value[] operands = null;
         int numOperands = splitParametersDescriptors(method.getDescriptor()).length;
 
@@ -237,5 +248,184 @@ public class InformationLogger {
         }
 
         md.setParameterSet(pSet);
+    }
+
+    private static void formatPathCondition(State s, StringBuilder sb)
+            throws FrozenStateException {
+        final StringBuilder expression = new StringBuilder();
+        final StringBuilder where = new StringBuilder();
+        boolean doneFirstExpression = false;
+        boolean doneFirstWhere = false;
+        HashSet<String> doneSymbols = new HashSet<String>();
+        for (Clause c : s.getPathCondition()) {
+            if (c instanceof ClauseAssume) {
+                expression.append(doneFirstExpression ? " &&" : "");
+                doneFirstExpression = true;
+                final Primitive cond = ((ClauseAssume) c).getCondition();
+                formatValue(s, expression, cond);
+                final StringBuilder expressionWhereCondition = new StringBuilder();
+                final boolean some = formatValueForPathCondition(cond, expressionWhereCondition, doneSymbols);
+                if (some) {
+                    where.append(doneFirstWhere ? " &&" : ""); where.append(expressionWhereCondition);
+                    doneFirstWhere = true;
+                } //else does nothing
+            } else if (c instanceof ClauseAssumeReferenceSymbolic) {
+                expression.append(doneFirstExpression ? " &&" : "");
+                doneFirstExpression = true;
+                final ReferenceSymbolic ref = ((ClauseAssumeReferenceSymbolic) c).getReference();
+                expression.append(ref.toString()); expression.append(" == ");
+                if (s.isNull(ref)) {
+                    expression.append("null");
+                } else {
+                    final ReferenceSymbolic tgtOrigin = s.getObject(ref).getOrigin();
+                    expression.append("Object["); expression.append(s.getResolution(ref)); expression.append("] ("); expression.append(ref.equals(tgtOrigin) ? "fresh" : ("aliases " + tgtOrigin)); expression.append(")");
+                }
+                final StringBuilder referenceFormatted = new StringBuilder();
+                final boolean someText = formatValueForPathCondition(ref, referenceFormatted, doneSymbols);
+                if (someText) {
+                    if (doneFirstWhere) {
+                        where.append(" &&");
+                    }
+                    where.append(referenceFormatted);
+                    doneFirstWhere = true;
+                }
+            } else {
+                if (!(c instanceof ClauseAssumeClassInitialized)) { //(c instanceof ClauseAssumeClassNotInitialized)
+                    expression.append(doneFirstExpression ? " &&" : "");
+                    doneFirstExpression = true;
+                    expression.append(c.toString());
+                }
+            }
+        }
+        if (expression.length() > 0) {
+            sb.append(expression);
+        }
+        if (where.length() > 0) {
+            sb.append("where:");
+            sb.append(where);
+        }
+    }
+
+    private static boolean formatExpressionForPathCondition(Expression e, StringBuilder sb, HashSet<String> done) {
+        final Primitive firstOp = e.getFirstOperand();
+        final Primitive secondOp = e.getSecondOperand();
+        boolean someFirstOp = false;
+        if (firstOp != null) {
+            someFirstOp = formatValueForPathCondition(firstOp, sb, done);
+        }
+        final StringBuilder second = new StringBuilder();
+        final boolean someSecondOp = formatValueForPathCondition(secondOp, second, done);
+        if (!someFirstOp || !someSecondOp) {
+            //does nothing
+        } else {
+            sb.append(" &&");
+        }
+        sb.append(second);
+        return (someFirstOp || someSecondOp);
+    }
+
+    private static boolean formatValueForPathCondition(Value v, StringBuilder sb, HashSet<String> done) {
+        if (v instanceof Expression) {
+            return formatExpressionForPathCondition((Expression) v, sb, done);
+        } else if (v instanceof PrimitiveSymbolicAtomic || v instanceof ReferenceSymbolicAtomic) {
+            if (done.contains(v.toString())) {
+                return false;
+            } else {
+                done.add(v.toString());
+                sb.append(v.toString()); sb.append(" == "); sb.append(((Symbolic) v).asOriginString());
+                return true;
+            }
+        } else if (v instanceof PrimitiveSymbolicApply) {
+            return formatFunctionApplicationForPathCondition((PrimitiveSymbolicApply) v, sb, done);
+        } else if (v instanceof ReferenceSymbolicApply) {
+            return formatFunctionApplicationForPathCondition((ReferenceSymbolicApply) v, sb, done);
+        } else if (v instanceof WideningConversion) {
+            final WideningConversion pWiden = (WideningConversion) v;
+            return formatValueForPathCondition(pWiden.getArg(), sb, done);
+        } else if (v instanceof NarrowingConversion) {
+            final NarrowingConversion pNarrow = (NarrowingConversion) v;
+            return formatValueForPathCondition(pNarrow.getArg(), sb, done);
+        } else { //(v instanceof DefaultValue || v instanceof Any || v instanceof Simplex || v instanceof Term || v instanceof ReferenceConcrete ||
+            // v instanceof ReferenceArrayImmaterial || v instanceof KlassPseudoReference)
+            return false;
+        }
+    }
+
+    private static boolean formatFunctionApplicationForPathCondition(PrimitiveSymbolicApply a, StringBuilder sb, HashSet<String> done) {
+        boolean some = false;
+        boolean firstDone = false;
+        for (Value v : a.getArgs()) {
+            final StringBuilder arg = new StringBuilder();
+            final boolean argSome = formatValueForPathCondition(v, arg, done);
+            some = some || argSome;
+            if (argSome) {
+                if (firstDone) {
+                    sb.append(" &&");
+                } else {
+                    firstDone = true;
+                }
+                sb.append(arg);
+            }
+        }
+        return some;
+    }
+
+    private static boolean formatFunctionApplicationForPathCondition(ReferenceSymbolicApply a, StringBuilder sb, HashSet<String> done) {
+        boolean first = true;
+        boolean some = false;
+        for (Value v : a.getArgs()) {
+            final StringBuilder arg = new StringBuilder();
+            final boolean argSome = formatValueForPathCondition(v, arg, done);
+            some = some || argSome;
+            if (argSome) {
+                //does nothing
+            } else {
+                if (!first) {
+                    sb.append(" &&");
+                }
+                sb.append(arg);
+                first = false;
+            }
+        }
+        return some;
+    }
+
+    private static void formatValue(State s, StringBuilder sb, Value val) {
+        if (val.getType() == Type.CHAR && val instanceof Simplex) {
+            final char c = ((Character) ((Simplex) val).getActualValue()).charValue();
+            if (c == '\t') {
+                sb.append("\\t");
+            } else if (c == '\b') {
+                sb.append("\\b");
+            } else if (c == '\n') {
+                sb.append("\\n");
+            } else if (c == '\f') {
+                sb.append("\\f");
+            } else if (c == '\r') {
+                sb.append("\\r");
+            } else if (c == '\"') {
+                sb.append("\\\"");
+            } else if (c == '\'') {
+                sb.append("\\\'");
+            } else if (c == '\\') {
+                sb.append("\\\\");
+            } else if (c == '\u0000') {
+                sb.append("\\u0000");
+            } else {
+                sb.append(c);
+            }
+        } else {
+            sb.append(val.toString());
+        }
+        if (val instanceof ReferenceSymbolic) {
+            final ReferenceSymbolic ref = (ReferenceSymbolic) val;
+            if (s.resolved(ref)) {
+                if (s.isNull(ref)) {
+                    sb.append(" == null");
+                } else {
+                    sb.append(" == Object["); sb.append(s.getResolution(ref)); sb.append("]");
+                }
+            }
+        }
     }
 }
