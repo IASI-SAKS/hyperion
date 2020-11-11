8a57d1c6d0f628a69607055b255957d2180cad8a
emadea <emanuele.de.angelis30@gmail.com>
2020-11-11 16:29:00 +0100

    brand new implementation of same_SET_of_invoked_methods/2

--- sim.pl
+++ sim.pl
@@ -1,51 +1,75 @@
-% same_SET_of_invoked_methods(+TP1,+TP2)
-% SEMANTICS: TP1 and TP2 invoke the same *set* of methods
-same_SET_of_invoked_methods(TP1,TP2) :-
-  set_of_invoked_methods(TP1,S1),
-  set_of_invoked_methods(TP2,S2),
-  S1 == S2.
+% NOTES: think about using
+% Ordered set manipulation:
+% https://www.swi-prolog.org/pldoc/man?section=ordsets
 
-% same_SET_of_invoked_methods(+TP1,+TP2)
-% SEMANTICS: TP1 and TP2 invoke the same *sequence* of methods
-same_SEQ_of_invoked_methods(TP1,TP2) :-
-  seq_of_invoked_methods(TP1,S1),
-  seq_of_invoked_methods(TP2,S2),
-  S1 == S2.
+% MODE: same_SET_of_invoked_methods(+TP1,+TP2)
+% SEMANTICS: the test programs TP1 and TP2 invoke the same *set* of methods
+% same_SET_of_invoked_methods(TP1,TP2) :-
+%   invoked_methods(TP1,S1),
+%   invoked_methods(TP2,S2),
+%   eq_set(S1,S2).
 
-% invoke template
+% MODE: invoked_methods(+TP,?L)
+% SEMANTICS: L is the list of methods invoked by the test program TP
+invoked_methods(TP,L) :-
+  % L is the list of all the instances of IM for which
+  % invoke(TP,_,_,_,_,IM,_) succeeds
+  findall(IM, invokes(TP,_,_,_,_,IM,_), L).
+
+% invokes/7 argument meaning
 % invokes(
-%   "nome" del test,
-%   branch point,
-%   sequence number,
-%   program point,
-%   path condition,
-%   metodo chiamato,
-%   parametri (WIP))
-% invoke instance:
-% invokes(
-%   com.fullteaching.backend.e2e.rest.UserRestTest:testCreateUserOk,
-%   [1],
-%   12,
-%   com/fullteaching/backend/e2e/rest/UserRestTest:()V@4,
-%   [({R0}, Object[4460] (fresh))], [({R0}, {ROOT}:this)],
-%   com/fullteaching/backend/e2e/rest/UserRestTest:createUser:(Ljava/lang/String;)I,
-%   [null, ])
+% 1  "nome" del test,
+% 2  branch point,
+% 3  sequence number,
+% 4  program point,
+% 5  path condition,
+% 6  metodo chiamato,
+% 7  parametri (WIP))
 
-set_of_invoked_methods(TP,Set) :-
-  % S is the list of all the instances of IM for which invoke(TP,_,_,_,_,IM,_) succeeds
-  findall(IM, invoke(TP,_,_,_,_,IM,_), S),
-  sort(S,Set). % sort removes duplicates
-  % alternative implementation:
-  % setof(IM, [BP,SN,PP,PC,PL]^invoke(TP,BP,SN,PP,PC,IM,PL), Set).
+% MODE: eq_set(+S1,+S2)
+% TYPE: eq_set(ground(list(term)),ground(list(term)))
+% SEMANTICS: eq_set(S1,S2) holds if S1 and S2 represent two equivalent sets.
+eq_set(S1,S2) :-
+  sub_set(S1,S2),
+  sub_set(S2,S1).
 
+% MODE: sub_set(+S1,+S2)
+% TYPE: sub_set(ground(list(term)),ground(list(term)))
+% SEMANTICS: sub_set(S1,S2) holds if all elements of S1 belong to S2.
+sub_set([],_).
+sub_set([S|S1],S2) :-
+  member(S,S2),
+  !,
+  sub_set(S1,S2).
 
-seq_of_invoked_methods(TP,Seq) :-
-  findall(SN-IM, invoke(TP,_,SN,_,_,IM,_), S),
-  keysort(S,SeqP),  % sort the list of pairs SN-IM by SN
-  rem1st(SeqP,Seq). % remove the 1st component from the elements in SeqP
-  % alternative implementation:
-  % findall(IM, member(_-IM,SeqP), Seq).
-%
-rem1st([],[]).
-rem1st([_-IM|L1],[IM|L2]) :-
-  rem1st(L1,L2).
+% "pretty" print utility predicate
+print_list([]).
+print_list([L|Ls]) :-
+  write(L), nl,
+  print_list(Ls).
+
+% % alternative implementation - v1
+% same_SET_of_invoked_methods(TP1,TP2) :-
+%   \+ exists_method_not_invoked(TP1,TP2),
+%   \+ exists_method_not_invoked(TP2,TP1).
+% % alternative implementation - v2
+% same_SET_of_invoked_methods(TP1,TP2) :-
+%   exists_method_not_invoked(TP1,TP2),
+%   !,
+%   fail.
+% same_SET_of_invoked_methods(TP1,TP2) :-
+%   exists_method_not_invoked(TP2,TP1),
+%   !,
+%   fail.
+% same_SET_of_invoked_methods(_TP1,_TP2).
+
+% MODE: exists_method_not_invoked(+TP1,+TP2)
+% SEMANTICS: there exists a method M invoked by TP1 that is not invoked by TP2
+exists_method_not_invoked(TP1,TP2) :-
+  tp_invokes_m(TP1,M),
+  \+ tp_invokes_m(TP2,M).
+
+% MODE: tp_invokes_m(?TP,?M)
+% SEMANTICS: TP invokes M
+tp_invokes_m(TP,M) :-
+  invokes(TP,_,_,_,_,M,_).
