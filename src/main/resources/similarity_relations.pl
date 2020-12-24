f4bf1554882a990ec92e1e2ae18d97ab9ec7da19
emadea <emanuele.de.angelis30@gmail.com>
2020-12-24 12:33:44 +0100

    minor changes

--- src/main/resources/similarity_relations.pl
+++ src/main/resources/similarity_relations.pl
@@ -1,3 +1,5 @@
+% reconsult('src/main/resources/similarity_relations.pl').
+
 :- set_prolog_flag(character_escapes,false).
 
 %% Checking similarity of test programs
@@ -70,23 +72,30 @@ invoked_methods(TP,L) :-
   findall(M, invokes(TP,_,_,_,_,_,_,M,_), L).
 
 % ------------------------------------------------------------------------------
-% SEMANTICS: invokeSequence(TP,Caller,Seq)
-% Seq is a sequence of direct invocations performed by Caller in TP
+% SEMANTICS: invokeSequence(TP,Caller,ISeq)
+% ISeq is a sequence of direct invocations performed by Caller in TP
 invokeSequence(TP,Caller,ISeq) :-
   Invokes = invokes(TP,_,_,Caller,_,_,_,_,_), Invokes,
   iseq(Invokes,ISeq).
 
-% SEMANTICS: invokeMaximalSequence(TP,Caller,Seq)
-% Seq is a maximal sequence of direct invocations performed by Caller in TP
+% SEMANTICS: invokeMaximalSequence(TP,Caller,ISeq)
+% ISeq is a maximal sequence of direct invocations performed by Caller in TP
+% TEST:
+% ?- invokeMaximalSequence('t3','m',Seq1), callees(Seq1,M1).
+% Seq1 = [invokes(t3, [1, 1], 1, m, 11, 3, [], a, []), invokes(t3, [1, 1], 3, m, 11, 3, [], b, []), invokes(t3, [1, 1], 5, m, 11, 3, [], c, [])],
+% M1 = [a, b, c] ;
+% Seq1 = [invokes(t3, [1, 2], 7, m, 11, 3, [], a, []), invokes(t3, [1, 2], 9, m, 11, 3, [], b, [])],
+% M1 = [a, b] ;
+% false.
 invokeMaximalSequence(TP,Caller,ISeq) :-
   method_first_invokes(TP,Caller,FirstInvokes),
   method_last_invokes(TP,Caller,LastInvokes),
-  FirstInvokes \= LastInvokes, % sequences of at least two method invocations
+  FirstInvokes \= LastInvokes, % sequences with at least two method invocations
   iseq(FirstInvokes,ISeq),
   append([FirstInvokes|_],[LastInvokes],ISeq).
 
 % SEMANTICS: iseq(I,ISeq)
-% ISeq is the sequence of invocations performed by Caller in TP starting from I
+% ISeq is a sequence of invocations performed by Caller in TP starting from I
 iseq(I1,[I1|Is]) :-
   I1 = invokes(TP,_,_,Caller,_,_,_,_,_),
   I2 = invokes(TP,_,_,Caller,_,_,_,_,_),
@@ -95,7 +104,7 @@ iseq(I1,[I1|Is]) :-
 iseq(I1,[I1]).
 
 % SEMANTICS: next(Invokes1,Invokes2)
-% Invokes2 is the subsequent invocation performed by Caller in TP
+% Invokes2 is a subsequent invocation performed by Caller in TP
 next(Invokes1,Invokes2) :-
   Invokes1 = invokes(TP,BP,SN1,Caller,_,_,_,_,_), Invokes1,
   Invokes2 = invokes(TP,BP,SN2,Caller,_,_,_,_,_), Invokes2,
@@ -111,43 +120,43 @@ next(Invokes1,Invokes2) :-
 % SEMANTICS: exists_intermediate_invokes(Invokes1,Invokes2) holds if
 % there exists an invokes InvokesM having the same TP and Caller of
 % Invokes1 and Invokes2, and
-% either it has the same branching point BP of Invokes1 and Invokes2 and
-% its seq.no. is greater than that of Invokes1 and less than that of Invokes2
+% either (a.1) it has the same branching point BP of Invokes1 and Invokes2, and
+% (a.2) its seq.no. SN is greater than the seq.no SN1 of Invokes1 and
+% less than the seq.no. SN2 of Invokes2
 exists_intermediate_invokes(Invokes1,Invokes2) :-
-  Invokes1 = invokes(TP,BP1,SN1,Caller,_,_,_,_,_),
-  Invokes2 = invokes(TP,BP2,SN2,Caller,_,_,_,_,_),
-  BP1 = BP2,
-  InvokesM = invokes(TP,BP2, SN,Caller,_,_,_,_,_), InvokesM,
-  SN1 < SN, SN < SN2,
+  Invokes1 = invokes(TP,BP,SN1,Caller,_,_,_,_,_),
+  Invokes2 = invokes(TP,BP,SN2,Caller,_,_,_,_,_),
+  InvokesM = invokes(TP,BP, SN,Caller,_,_,_,_,_), InvokesM, % (a.1)
+  SN1 < SN, SN < SN2,                                       % (a.2)
   !.
-% or its branching point BP extends the branching point of Invokes1, and
-% it is a prefix of the branching point BP2 of Invokes2 such that either
-% (*) BP2 extends BP or (**) BP = BP2 with a seq.no. less than that of Invokes2
+% or (b.1) its branching point BP extends the branching point BP1 of Invokes1,
+% and (b.2) BP is a prefix of the branching point BP2 of Invokes2, and either
+% (b.2.1) BP2 extends BP, or
+% (b.2.2) BP2 = BP and a seq.no. SN less than the seq.no. SN2 of Invokes2
 exists_intermediate_invokes(Invokes1,Invokes2) :-
   Invokes1 = invokes(TP,BP1,  _,Caller,_,_,_,_,_),
   Invokes2 = invokes(TP,BP2,SN2,Caller,_,_,_,_,_),
   BP1 \= BP2,
   InvokesM = invokes(TP,BP,  SN,Caller,_,_,_,_,_), InvokesM,
-  append(BP1,[_|_],BP), append(BP,Infix,BP2),
-  ( Infix = [_|_] % (*)
+  append(BP1,[_|_],BP),      % (b.1)
+  append(BP,Infix,BP2),      % (b.2)
+  ( Infix = [_|_]            % (b.2.1)
   ;
-    ( Infix = [], SN < SN2 ) % (**)
+    ( Infix = [], SN < SN2 ) % (b.2.2)
   ).
 
 % Invokes is the first invocation performed by Caller in TP
 method_first_invokes(TP,Caller,Invokes) :-
   Invokes = invokes(TP,_,_,Caller,_,_,_,_,_), Invokes,
-  \+ exist_preceeding_invokes(Invokes).
+  \+ exists_preceeding_invokes(Invokes).
 % method_first_invokes utility predicate
-exist_preceeding_invokes(Invokes) :-
-  next(_,Invokes).
+exists_preceeding_invokes(Invokes) :- next(_,Invokes).
 % Invokes is the last invocation performed by Caller in TP
 method_last_invokes(TP,Caller,Invokes) :-
   Invokes = invokes(TP,_,_,Caller,_,_,_,_,_), Invokes,
-  \+ exist_succeeding_invokes(Invokes).
+  \+ exists_succeeding_invokes(Invokes).
 % method_last_invokes utility predicate
-exist_succeeding_invokes(Invokes) :-
-  next(Invokes,_).
+exists_succeeding_invokes(Invokes) :- next(Invokes,_).
 
 % SEMANTICS: callees(Is,Ms)
 % Ms is the list of calee occurring in the list of invokes Is
