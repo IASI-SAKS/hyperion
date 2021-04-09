d21df6af830c94053d5925395bd1aa2e507bf990
emadea <emanuele.de.angelis30@gmail.com>
2021-04-09 15:54:52 +0200

    - added trace/2 for generating symbolic execution traces - added similar_endpoints/6 for detecting similar sets of endpoints - minor change to README.md

--- src/main/prolog/testing_similarity_relations.pl
+++ src/main/prolog/testing_similarity_relations.pl
@@ -80,12 +80,47 @@ print_atom(A) :-
   ( As == [] ->
     write('.')
   ;
-    ( write('('), nl, print_atom_args(As), nl, write(').') )
+    ( write('('), nl, print_qatom_args(As), nl, write(').') )
   ).
 %
 print_atom_args([L]) :-
   write(' '), write(L).
 print_atom_args([L|Ls]) :-
   write(' '), write(L), nl,
+  write(' '), write(L), write(','), nl,
   !,
   print_atom_args(Ls).
+%
+print_qatom_args([L]) :-
+  write(' '''), write(L), write('''').
+print_qatom_args([L|Ls]) :-
+  write(' '''), write(L), write(''','), nl,
+  !,
+  print_qatom_args(Ls).
+
+% calls similar_endpoints/4 and prints its answer
+print_similar_endpoints_answer(EndpointsSrc,Criterion) :-
+  write('--------------------------------------------------------------------------------'), nl,
+  write('Querying similar_endpoints/6'), nl, nl,
+  write('* Endpoints source: '), write(EndpointsSrc), nl,
+  write('* Criterion:        '), write(Criterion), nl, nl,
+  similar_endpoints(EndpointsSrc,Criterion,TP1,TP2,Es1,Es2),
+  write('* Test Program 1:   '), write(TP1), nl, nl,
+  print_atom_list(Es1),                     nl,
+  write('* Test Program 2:   '), write(TP2), nl, nl,
+  print_atom_list(Es2), nl.
+%
+print_similar_endpoints(Criterion) :-
+  generate_and_assert_endpoints(Criterion),
+  atom_concat('similar_endpoints-',Criterion,FileNamePrefix),
+  atom_concat(FileNamePrefix,'-report.txt',FileName),
+  tell(FileName),
+  print_similar_endpoints_answers(Criterion,eqset),
+  print_similar_endpoints_answers(Criterion,subset),
+  print_similar_endpoints_answers(Criterion,nonempty_intersection),
+  told.
+%
+print_similar_endpoints_answers(EndpointsSrc,Criterion) :-
+  print_similar_endpoints_answer(EndpointsSrc,Criterion),
+  fail.
+print_similar_endpoints_answers(_EndpointsSrc,_Criterion).
